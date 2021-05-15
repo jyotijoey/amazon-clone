d5a897caf4cb2a00b56bb6d0bde803ff6f61d0a6
jyotijoey <jburnwal5@gmail.com>
2021-05-16 01:20:10 +0530

    added the add-to-basket functionality

--- src/Header.js
+++ src/Header.js
@@ -3,8 +3,10 @@ import "./Header.css"
 import Searchicon from '@material-ui/icons/Search';
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 import { Link } from "react-router-dom";
+import { useStateValue } from './StateProvider';
 
 function Header() {
+    const [{basket}, dispatch] = useStateValue();
     return (
         <div className="header">
 
@@ -37,7 +39,7 @@ function Header() {
                 <Link to="/checkout">
                     <div className="header_optionBasket">
                         <ShoppingCartIcon />
-                    <span className="header_optionLineTwo header_basketCount">0</span>
+                    <span className="header_optionLineTwo header_basketCount">{basket?.length}</span>
                 </div>
                 </Link>
             </div>
--- src/Product.js
+++ src/Product.js
@@ -1,7 +1,26 @@
 import React from 'react'
 import "./Product.css"
+import {useStateValue} from './StateProvider';
 
 function Product({id, title, image, price, rating}) {
+   const [{basket}, dispatch] = useStateValue();
+
+   const addToBasket = () =>{
+       //dispatch item to the data layer
+       console.log("this is the item in the basket", basket);
+       dispatch({
+           type: "ADD_TO_BASKET",
+           item: {
+               id: id,
+               title: title,
+               image: image,
+               price: price,
+               rating: rating
+           },
+
+       });
+   };
+   
     return (
         <div className="product">
             <div className="product_info">
@@ -21,7 +40,7 @@ function Product({id, title, image, price, rating}) {
                 </div>
                 <img src={image} alt="The lean startup book" />
 
-                <button>Add To Cart</button>
+                <button onClick={addToBasket}>Add To Basket</button>
             
         </div>
     )
