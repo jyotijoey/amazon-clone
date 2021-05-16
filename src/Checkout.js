9251b66c2f69c94d1075207c81f7423265dea9cd
jyotijoey <jburnwal5@gmail.com>
2021-05-16 13:39:49 +0530

    added shopping basket product list and remove from basket functionality to the checkout page

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import "./Checkout.css"
+import CheckoutProduct from './CheckoutProduct'
+import { useStateValue } from './StateProvider';
 import Subtotal from "./Subtotal"
 
 function Checkout() {
+    const [{basket}, dispatch] = useStateValue();
+    console.log(basket);
     return (
         <div className="checkout">
         {/* left side */}
@@ -10,9 +14,18 @@ function Checkout() {
                 <img className="checkout_ad" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcScJ0H7vg-i_rwJvM2ICFuzzBuzN3HzYCyIqA&usqp=CAU" alt="checkout ad" />
                 <div>
                     <h2 className="checkout_title">
-                        Your Shopping Basket
-                        {/* basket item */}
+                        Your Shopping basket
                     </h2>
+                    {basket.map(item =>( 
+                    <CheckoutProduct 
+                        id={item.id}
+                        image={item.image}
+                        title={item.title}
+                        price={item.price}
+                        rating={item.rating}
+                    />
+                    ))}
+
                 </div>
             </div>
             {/* right side */}
